e1626f610598167a007e10a2a09af1dfabe08a92
anders tungland <a-tunglan@hotmail.com>
2022-09-19 10:30:16 +0200

    fikset filtyper som er lov å laste opp

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,13 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app, query_db
 from app.forms import IndexForm, PostForm, FriendsForm, ProfileForm, CommentsForm
 from datetime import datetime
+# from egnefunksjoner import fileType
 import os
+
+# functions
+def fileType(filename):
+    fil = filename.split(".")[-1]
+    return fil
 
 # this file contains all the different routes, and the logic for communicating with the database
 
@@ -34,13 +40,17 @@ def stream(username):
     form = PostForm()
     user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
     if form.is_submitted():
+        filtype = fileType(form.image.data.filename)
+        okFiler = ['png', 'jpeg', 'jgp']
         if form.image.data:
             path = os.path.join(app.config['UPLOAD_PATH'], form.image.data.filename)
             form.image.data.save(path)
 
-
-        query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, form.image.data.filename, datetime.now()))
-        return redirect(url_for('stream', username=username))
+        if filtype in okFiler:
+            query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, form.image.data.filename, datetime.now()))
+            return redirect(url_for('stream', username=username))
+        else:
+            flash("sorry this file format is not allowed")
 
     posts = query_db('SELECT p.*, u.*, (SELECT COUNT(*) FROM Comments WHERE p_id=p.id) AS cc FROM Posts AS p JOIN Users AS u ON u.id=p.u_id WHERE p.u_id IN (SELECT u_id FROM Friends WHERE f_id={0}) OR p.u_id IN (SELECT f_id FROM Friends WHERE u_id={0}) OR p.u_id={0} ORDER BY p.creation_time DESC;'.format(user['id']))
     return render_template('stream.html', title='Stream', username=username, form=form, posts=posts)
